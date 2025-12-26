4afaab9482c476393a0588ec1cd2a63ec6056336
Florian Verdonck <florian.verdonck@outlook.com>
2025-12-26 11:36:15 +0100

    Remove glob dependency (#8115)
    * Remove glob dependency
    
    * Collect files
    
    * check input array

--- packages/playground/rollup.config.mjs
+++ packages/playground/rollup.config.mjs
@@ -1,6 +1,6 @@
 import * as path from "node:path";
 import nodeResolve from "@rollup/plugin-node-resolve";
-import { glob } from "glob";
+import { glob } from "node:fs/promises";
 
 const RESCRIPT_COMPILER_ROOT_DIR = path.join(import.meta.dirname, "..", "..");
 const LIB_DIR = path.join(RESCRIPT_COMPILER_ROOT_DIR, "packages", "@rescript", "runtime", "lib");
@@ -9,9 +9,18 @@ const LIB_DIR = path.join(RESCRIPT_COMPILER_ROOT_DIR, "packages", "@rescript", "
 const PACKAGES_DIR = path.join(import.meta.dirname, "packages");
 const outputFolder = path.join(PACKAGES_DIR, "compiler-builtins", "stdlib");
 
-const entryPoint = await glob(`${LIB_DIR}/es6/*.js`);
+let input = [];
+
+for await (const file of glob(`${LIB_DIR}/es6/*.js`, { withFileTypes: false })) {
+  input.push(file);
+}
+
+if (input.length === 0) {
+  throw new Error("No entry points found in playground");
+}
+
 export default {
-  input: entryPoint,
+  input,
   output: {
     dir: outputFolder,
     format: "esm",
