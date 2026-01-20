20b076085d2066f8bdb5539efbcad2ef815d4469
Paul Tsnobiladzé <paul.tsnobiladze@gmail.com>
2026-01-20 12:33:41 +0100

    use Rust 2-1-trixie base and OPAM 2.5.0 (#8199)
    * use Rust 2-1-trixie base and OPAM 2.5.0
    
    * check that devcontainer can run make
    
    * install ninja-build for binaryen-bin
    
    * install packages too
    
    * run opam init with auto-setup

--- .devcontainer/postCreate.sh
+++ .devcontainer/postCreate.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 # Install dev dependencies from OPAM
-opam init -y --bare --disable-sandboxing
+opam init -y --auto-setup --bare --disable-sandboxing
 opam switch create 5.3.0
-opam install . --deps-only --with-test --with-dev-setup -y
+opam install . --with-test --with-dev-setup -y
 
 # Add OPAM environment setup to shell startup script
 echo 'eval $(opam env)' >> ~/.zshrc
